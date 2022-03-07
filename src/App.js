80c9c6c9c281dee1f6ed04b3ad2b87c437124789
iamLolade <adeniyiotemade@gmail.com>
2022-03-08 00:50:01 +0100

    Setup components routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Notes from "./pages/Notes"
+import Create from "./pages/Create"
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Notes />} />
+          <Route path="/create" element={<Create />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

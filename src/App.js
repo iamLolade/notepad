aa7409cc7854c8661c71ca5e9d4c15c9e7f97217
iamLolade <adeniyiotemade@gmail.com>
2022-03-08 04:02:21 +0100

    Created custom theme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Notes from "./pages/Notes"
 import Create from "./pages/Create"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#fefefe"
+    }
+  }
+})
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Notes />} />
-          <Route path="/create" element={<Create />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Notes />} />
+            <Route path="/create" element={<Create />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,6 +2,9 @@ import { Typography } from "@mui/material";
 import { Button, Container } from "@mui/material";
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
+// const styles = {
+//     fontSize: 90
+// }
 
 const Create = () => {
     return ( 
@@ -15,6 +18,7 @@ const Create = () => {
                 Create a New Note
             </Typography>
             <Button 
+                // sx={styles}
                 variant="contained" 
                 color="primary"
                 endIcon={<KeyboardArrowRightIcon />}
@@ -25,3 +29,14 @@ const Create = () => {
 }
  
 export default Create;
+
+
+// very simple just define this object
+//  const styles = {
+//         fontSize= 80
+//     }
+
+// then pass this  prop like here in this button 
+// <Button
+// sx={styles}
+// >"This is the prop for styling"</Button>

b2410fe148c59ca5c422750836bb74573007f9b9
iamLolade <adeniyiotemade@gmail.com>
2022-03-12 20:43:40 +0100

    Updated NoteCard component | Created card layout for Notes

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,10 +1,44 @@
-import { Card, CardHeader, CardActions, Collapse, Avatar } from "@mui/material";
+import { useState } from "react";
+import { DeleteOutlined, ExpandMore } from "@mui/icons-material";
+import { Card, CardHeader, CardActions, CardContent, Collapse, Avatar, Typography, IconButton } from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-const NoteCard = ({note}) => {
+const NoteCard = ({ note, handleDelete}) => {
+    const [expanded, setExpanded] = useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
     return ( 
-        <div>
-            { note.title }
-        </div>
+        <Card elevation={1}>
+            <CardHeader
+                action={
+                    <IconButton>
+                        <DeleteOutlined onClick={() => handleDelete(note.id)} />
+                    </IconButton>
+                }
+                title={ note.title }
+                subheader={ note.category }
+            />
+            <CardActions disableSpacing>
+                <ExpandMore
+                    expand={expanded}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                >
+                    <ExpandMoreIcon />
+                </ExpandMore>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary">
+                        { note.details }
+                    </Typography>
+                </CardContent>
+            </Collapse>
+        </Card>
      );
 }
  
--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -12,12 +12,20 @@ const Notes = () => {
             .then(data => setNotes(data))
     }, [])
 
+    const handleDelete = async (id) => {
+        await fetch(`http://localhost:8000/notes/${id}`, {
+            method: "DELETE"
+        })
+        const newNotes = notes.filter(note => note.id != id)
+        setNotes(newNotes)
+    }
+
     return (
         <Container>
-            <Grid container> 
+            <Grid container spacing={3}> 
                     {notes.map(note => (
                         <Grid item xs={12} md={6} lg={4} key={note.id}>
-                            <NoteCard note={note}/>
+                            <NoteCard note={note} handleDelete={handleDelete}/>
                         </Grid>
                     ))}
             </Grid>
